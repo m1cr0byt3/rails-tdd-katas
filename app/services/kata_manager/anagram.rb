75e706f0fddb1f4154a511258e3b8b065a52c9ba
Jonathan Byte <jonathan.cruzpalacios@outlook.com>
2022-04-27 21:05:19 -0600

    Anagram

--- app/services/kata_manager/anagram.rb
+++ app/services/kata_manager/anagram.rb
@@ -65,16 +65,19 @@ module KataManager
 
     def match_next(match)
       rest_chars_total = @word_base.size - match.size
-      if @list_words[@next_index].size == rest_chars_total
-        order_next = order_word(@list_words[@next_index])
-        next_match = order_next.match(/#{@rest_match}/).to_a.first
-        save_matchs(next_match, match) if next_match.present? && remaining_word(match) == order_next
-      end
+      find_next_match(match) if @list_words[@next_index].size == rest_chars_total
       @next_index += 1
     end
 
-    def save_matchs(_next_match, _match)
-      binding.pry
+    def find_next_match(match)
+      order_next = order_word(@list_words[@next_index])
+      next_match = order_next.match(/#{@rest_match}/).to_a.first
+      save_matchs(next_match, match) if next_match.present? && remaining_word(match) == order_next
+    end
+
+    def save_matchs(next_match, match)
+      @data[:matches] << { first: match, second: next_match }
+      @success = true
     end
 
     def remaining_word(match)
